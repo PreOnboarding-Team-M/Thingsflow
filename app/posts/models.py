796cd9efde9c24d80ac3f3abbdcb704615aa5917
Jungminchae <minchae3618@gmail.com>
2022-09-07 00:25:59 +0900

    Feat: Create Post model

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,11 @@
-from django.db import models  # noqa
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=20, verbose_name=_("제목"))
+    body = models.CharField(max_length=200, verbose_name=_("본문"))
+    password = models.CharField(max_length=255, verbose_name=_("비밀번호"))
+
+    def __str__(self):
+        return self.title
