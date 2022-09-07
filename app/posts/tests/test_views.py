ea2d53a51c035619979e3b07a3e8b2b56b053dbc
Jungminchae <minchae3618@gmail.com>
2022-09-07 12:44:27 +0900

    Test: Add Post delete cases

--- app/posts/tests/test_views.py
+++ app/posts/tests/test_views.py
@@ -21,13 +21,27 @@ def test_post_update_success(client):
     """Post 수정 테스트 - 성공"""
     post = Post.objects.create(title="제목", body="본문", password="123abc")
     data = {"title": "제목 수정", "body": "본문 수정", "password": "123abc"}
-    url = reverse("posts:post-update", kwargs={"pk": post.pk})
+    url = reverse("posts:post-retrieve-update-delete", kwargs={"pk": post.pk})
 
     response = client.patch(url, data=data, content_type="application/json")
 
     assert response.status_code == 200
     assert response.data["title"] == "제목 수정"
     assert response.data["body"] == "본문 수정"
+
+
+def test_post_update_fail_with_wrong_password(client):
+    """Post 수정 테스트 - 실패
+    비밀번호 틀림
+    """
+    post = Post.objects.create(title="제목", body="본문", password="123abc")
+    data = {"title": "제목 수정", "body": "본문 수정", "password": "123abcd"}
+    url = reverse("posts:post-retrieve-update-delete", kwargs={"pk": post.pk})
+
+    response = client.patch(url, data=data, content_type="application/json")
+
+    assert response.status_code == 400
+    assert response.data["password"] == ["비밀번호가 일치하지 않습니다."]
 
 
 def test_post_list_success_with_only_20_posts(client, sample_posts):
@@ -54,3 +68,30 @@ def test_post_list_success_with_left_10_posts(client, sample_posts):
 
     assert response.status_code == 200
     assert len(response.data["results"]) == length
+
+
+def test_post_delete_success(client):
+    """Post 삭제 테스트 - 성공"""
+    post = Post.objects.create(title="제목", body="본문", password="123abc")
+    url = reverse("posts:post-retrieve-update-delete", kwargs={"pk": post.pk})
+
+    response = client.delete(
+        url, data={"password": "123abc"}, content_type="application/json"
+    )
+
+    assert response.status_code == 204
+
+
+def test_post_delete_fail_with_wrong_password(client):
+    """Post 삭제 테스트 - 실패
+    비밀번호 틀림
+    """
+    post = Post.objects.create(title="제목", body="본문", password="123abc")
+    url = reverse("posts:post-retrieve-update-delete", kwargs={"pk": post.pk})
+
+    response = client.delete(
+        url, data={"password": "123abcd"}, content_type="application/json"
+    )
+
+    assert response.status_code == 400
+    assert response.data["password"] == ["비밀번호가 일치하지 않습니다."]
